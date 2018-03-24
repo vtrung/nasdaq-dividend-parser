534ee9a7ee6ae2408e70dfefb3cc5d7c8647d74b
Ving <svingt@gmail.com>
2018-03-24 16:42:11 +0000

    Adding multithreading into program

--- README.md
+++ README.md
@@ -1,15 +1,33 @@
 # **nasdaq-dividend-parser**
----
 
 ### Purpose:
-display the important dividend information about a stock
+Display  important dividend information about a stock
 
 ### Function:
-parses latest dividend information from nasdaq.com
+Parses latest dividend information from nasdaq.com
 
 ### Usage:
+
 python3 pardiv.py [stock symbols...]
-i.e: python3 pardiv.py AAPL GE IBM
+
+Multithreading Options [-m]
+
+i.e: python3 pardiv.py -m AAPL GE IBM
+>"Symbol: AAPL
+Ex-dividend Date: 2/9/2018
+Payment Date: 2/15/2018
+Dividend: $0.63
+===========
+Symbol: GE
+Ex-dividend Date: 2/23/2018
+Payment Date: 4/25/2018
+Dividend: $0.12
+===========
+Symbol: IBM
+Ex-dividend Date: 2/8/2018
+Payment Date: 3/10/2018
+Dividend: $1.5
+==========="
 
 ### Dependencies:
 [BeautifulSoup4](https://www.crummy.com/software/BeautifulSoup/)
--- pardiv.py
+++ pardiv.py
@@ -1,6 +1,7 @@
 from sys import argv
 from bs4 import BeautifulSoup
 import requests
+import threading
 
 class stock:
 	symbol = ""
@@ -31,24 +32,51 @@ class stock:
 		print("Ex-dividend Date:", self.exd)
 		print("Payment Date:", self.payd)
 		print("Dividend: $", self.div, sep='')
-		
+
+
+class reqthread(threading.Thread):
+    def __init__(self,symbol):
+        threading.Thread.__init__(self)
+        self.symbol = symbol
+    def run(self):
+        self.stock = stock(self.symbol)
+        self.stock.get()
+    def getstock(self):
+        return self.stock
+
 
 if __name__ == "__main__":
 	if len(argv) > 1:
+                #multi define if program will run in parallel or sequentially
+                multi = False;
                 symbols = argv[1:]
+                threads = []
                 for symbol in symbols:
-                    s = stock(symbol)
-                    s.get()
-                    s.print()
+                    if symbol[0] == '-':
+                        if symbol == "-m":
+                            multi = True
+                        continue
+                    th = reqthread(symbol)
+                    threads.append(th)
+                
+                #start threads: if multi is true, run parallel. if false, run sequentially
+                for t in threads:
+                    t.start()
+                    if not multi:
+                        t.join()
+
+                if multi:
+                    for t in threads:
+                        t.join()
+                
+                for t in threads:
+                    stock = t.getstock()
+                    stock.print()
                     print("===========")
+
+
 	else:
                 print("No arguments found")
                 print("Please pass in any number of stock symbol you wish to lookup")
                 print("Example: python3 pardiv.py IBM AAPL GE")
-		#tickers = ["GE","IBM", "AAPL"]
-		#for sym in tickers:
-		#	s = stock(sym)
-		#	s.get()
-		#	s.print()
-		#	print("============")
-	
+
